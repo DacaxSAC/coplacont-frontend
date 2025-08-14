54e1452547d93e7f806fd1551a01af9c56d945fe
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-08-14 16:18:40 -0500

    feat(kardex): implement kardex table view with mock data
    Add table component to display kardex inventory movements with placeholder data
    Prepare structure for future API integration

--- src/domains/inventory/pages/kardex/MainPage.tsx
+++ src/domains/inventory/pages/kardex/MainPage.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
-import { UnderConstruction } from '@/components';
+import { useState, useEffect } from "react";
+import { PageLayout, Button, Table } from "@/components";
+import type { InventoryItem } from "../../services/types";
 
 export const MainPage: React.FC = () => {
+
+  //Por ahora
+  const [inventory, setInventory] = useState<InventoryItem[]>([]);
+
+
+  useEffect(() => {
+    //Logica para cargar kardex
+  }, []);
+
+  const headers = [
+    "Fecha",
+    "Tipo",
+    "Tipo de comprobante",
+    "Cod de comprobante",
+    "Cantidad",
+    "Saldo",
+    "Costo Unitario",
+    "Costo Total",
+  ];
+
+  //Actualizar cuando se consuma el endpoint dekardex
+  const rows = inventory.map((i) => ({
+    id: i.id,
+    cells: [
+      i.almacen.id,
+      i.almacen.nombre,
+      i.producto.codigo,
+      i.producto.descripcion,
+      i.stockActual,
+        <Button
+          size="tableItemSize"
+          variant="tableItemStyle"
+          onClick={() => {}}
+        >
+          Ver KARDEX
+        </Button>
+
+    ],
+  }));
+  const gridTemplate = "1.5fr 1.5fr 1.5fr 1.5fr 1fr 1fr 1fr 1fr";
+
+
   return (
-    <UnderConstruction
-      title="Submodulo de kardex"
-      message="El submodulo de kardex está en construcción. Por favor, regresa pronto."
-    />
+     <PageLayout
+      title="Kardex"
+      subtitle="Muestra el detalle de movimientos y saldos del producto seleccionado."
+    >
+      <Table headers={headers} rows={rows} gridTemplate={gridTemplate} />
+    </PageLayout>
   );
 };
