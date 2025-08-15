d1a4dbc924ab05b02e012299dc53579f21aa7bfb
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-08-14 19:04:18 -0500

    feat(maintainers): add FormWarehouse and FormEntidad exports
    Add new exports for FormWarehouse and FormEntidad components to maintainers organisms

--- src/domains/maintainers/organisms/index.ts
+++ src/domains/maintainers/organisms/index.ts
@@ -2,3 +2,5 @@ export { CategoryModal } from './CategoryModal';
 export { ProductModal } from './ProductModal';
 export { ServiceModal } from './ServiceModal';
 export { WarehouseModal } from './WarehouseModal';
+export { FormEntidad } from './FormEntidad';
+export { FormWarehouse } from './FormWarehouse';
